605af7f48dcdbc465e91ca0a430dffed1a0c7c64
David Gogua <tortex0.60@gmail.com>
2025-04-08 21:48:13 +0400

    feat: add skeleton component

---
+++ src/app/components/skeleton/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import styles from "./Skeleton.module.scss";
+
+export interface SkeletonProps {
+  variant?: "small" | "medium" | "large";
+  className?: string;
+}
+
+const Skeleton: React.FC<SkeletonProps> = ({
+  variant = "medium",
+  className = "",
+}) => {
+  return <div className={`${styles.skeleton} ${styles[variant]} ${className}`} />;
+};
+
+export default Skeleton;
